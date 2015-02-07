15e6e2429b4238ab02fcfab460c5f7c682390057
Marcin Michałek <marcin.marcim@gmail.com>
2015-02-07 20:21:30 +0100

    project init

---
+++ src/main/java/Application.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Marcin on 2015-02-07.
+ */
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+    }
+}
