86df28f338ffb2da8613c754828dce9f36d67969
marcin-michalek <marcin.konrad.michalek@hotmail.com>
2015-09-07 20:54:12 +0200

    Update file headers

--- src/main/java/pl/michalek/marcin/piserver/network/weather/WeatherUtil.java
+++ src/main/java/pl/michalek/marcin/piserver/network/weather/WeatherUtil.java
@@ -3,9 +3,13 @@ package pl.michalek.marcin.piserver.network.weather;
 import pl.michalek.marcin.piserver.config.Constants;
 import pl.michalek.marcin.piserver.network.BaseRestTemplate;
 import pl.michalek.marcin.piserver.network.dto.DailyForecastData;
+import pl.michalek.marcin.piserver.network.dto.openweather.DailyForecast;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
- * SvnMirror
+ * PiServer
  * Created by Marcin on 2015-07-18.
  */
 public class WeatherUtil extends BaseRestTemplate {
